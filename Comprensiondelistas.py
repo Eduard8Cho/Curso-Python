c3809e081df175c82d1a6a16da6540a23c0e11ca
EduardoCGarcia <echavezg004@gmail.com>
2021-06-19 16:02:33 -0500

    Validaciones

--- Comprensiondelistas.py
+++ Comprensiondelistas.py
@@ -1,17 +1,16 @@
 datos=[0,1,2,3,4]
 print(datos)
-datos2=[i for i in range(1,51)]
+datos2=[i for i in range(1,51)] #La lista se llena sola con los numeros del 1 al 5
 print(datos2)
 
 multiplosde5=[i for i in range(1,100) if i%5==0]
 print(multiplosde5)
 
 
-
 print(ord('A'))#codifo ascci
 print(chr(65))#codigo ascci
 
-ABCDARIO=[chr(i) for i in range(65,91)]
+ABCDARIO=[chr(i) for i in range(65,91)] #Recorriendo como numeros para imprimir el caracter
 print(ABCDARIO)
 
 #lista de listas 
